86d4eceb34e6322fb33f2b321253dbc1f869ed23
Nathan Sizemore <nathanrsizemore@gmail.com>
2016-06-23 13:29:42 -0400

    Updated docs.

--- simple_stream/socket/sidebar-items.js
+++
@@ -1,1 +0,0 @@
-initSidebarItems({"struct":[["Socket","Wrapper for file descriptor based sockets"]],"trait":[["SocketOptions","The `SocketOptions` trait allows for various socket level settings."],["TcpOptions","The `TcpOptions` trait allows for various TCP level settings."]]});
